8de803cc151e76e91f73524f718c9997f7fb94ce
anhpt0920 <panhtokusatsu@gmail.com>
2025-06-10 21:09:04 +0700

    Update ChessMain.py

--- src/ChessMain.py
+++ src/ChessMain.py
@@ -1,6 +1,3 @@
-"""
-This  is our main driver file. It will be responsible for handling user input and displaying the current GameState object.
-"""
 from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame as p
@@ -15,9 +12,6 @@ SQ_SIZE = BOARD_HEIGHT // DIMENTIONS
 MAX_FPS = 16
 IMAGES = {}
 
-'''
-Initialize a global dictionary of images. This will be called exactly once in the main
-'''
 def loadImages():
     # Normal board
     pieces = ["wR", "wN", "wB", "wQ", "wK", "wP", "bR", "bN", "bB", "bQ", "bK", "bP"]
@@ -25,10 +19,6 @@ def loadImages():
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("images/"+theme+piece+".png"), (SQ_SIZE, SQ_SIZE))
 
-
-"""
-The main drive of our code. This will handle user input and updating the graphics
-"""
 def main():
     p.init()
     p.display.set_caption("Chess")
@@ -38,26 +28,19 @@ def main():
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
 
-    # Move log font
     moveLogFont = p.font.SysFont("Arial", 14, False, False)
     
-    # Generate validmoves
     validMoves = gs.getValidMoves()
     moveMade = False
 
-    # Only loads once before while loop
     loadImages()
     running=True
-
-    # No square selected yet, keep track of player clicks
     sqSelected = ()
-
-    # Keop track of player click
     playerClicks = []
 
     gameOver = False
-    playerOne = True # If human playing white then player one is true and vice versa
-    playerTwo = False # Same as above
+    playerOne = True
+    playerTwo = False
 
     AIThinking = False
     moveFinderProcess = None
@@ -74,7 +57,6 @@ def main():
                     location = p.mouse.get_pos()
                     col = location[0]//SQ_SIZE
                     row = location[1]//SQ_SIZE
-                    # The user clicked the mouse twice or user clicked mouse log
                     if sqSelected == (row, col) or col >= 8:
                         sqSelected = ()
                         playerClicks = []
@@ -95,7 +77,6 @@ def main():
                         whoIsPlaying = "P2" if gs.whiteToMove else "P1"
                         print(whoIsPlaying, "move: ", move.getChessNotation(gs.moveLog))
 
-            # Key handler
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z:
                     gs.undoMove()
@@ -118,13 +99,11 @@ def main():
                         AIThinking = False
                     moveUndone = True
         
-        # AI move finder
         if not gameOver and not humanTurn and not moveUndone:
             if not AIThinking:
                 AIThinking = True
                 print("Thinking...")
 
-                # Use this to pass data between threads
                 returnQueue = Queue()
                 moveFinderProcess = Process(target=ChessAI.findBestMove, args=(gs, validMoves, returnQueue))
                 moveFinderProcess.start()
@@ -154,18 +133,12 @@ def main():
         clock.tick(MAX_FPS)
         p.display.flip()
 
-'''
-Responsible for all the graphic within a current game state
-'''
 def drawGameState(screen, gs, validMoves, sqSelected, moveLogFont):
     drawBoard(screen)
     highlightSquares(screen, gs, validMoves, sqSelected)
     drawPieces(screen, gs.board)
     drawMoveLog(screen, gs, moveLogFont)
 
-'''
-Draw the square
-'''
 def drawBoard(screen):
     WHITE = (237, 238, 209)
     GREEN = (119, 153, 82)
@@ -176,23 +149,17 @@ def drawBoard(screen):
             color = colors[((r+c)%2)]
             p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
     
-    # Display Files and Ranks:
-        # Display numbers only on the "a" file
         font = p.font.SysFont('comicsans', 14, bold=True)
         text = font.render(str(8 - r), True, colors[0] if r % 2 == 1 else colors[1])
         text_rect = text.get_rect(center=(SQ_SIZE // 8, r * SQ_SIZE + SQ_SIZE // 4))
         screen.blit(text, text_rect)
         
-    # Display file letters on the top
     for c in range(DIMENTIONS):
         font = p.font.SysFont('comicsans', 18)
         text = font.render(chr(97 + c), True, colors[0] if c % 2 == 0 else colors[1])
         text_rect = text.get_rect(bottomright=(c * SQ_SIZE + SQ_SIZE - SQ_SIZE / 32, 8 * SQ_SIZE))
         screen.blit(text, text_rect)
 
-'''
-Hightlight square selected and moves for piece selected
-'''
 def highlightSquares(screen, gs, validMoves, sqSelected):
     if sqSelected != ():
         r, c = sqSelected
@@ -208,22 +175,15 @@ def highlightSquares(screen, gs, validMoves, sqSelected):
                 if move.startRow == r and move.startCol == c:
                     drawDot(screen, (move.endCol*SQ_SIZE + SQ_SIZE//2, move.endRow*SQ_SIZE + SQ_SIZE//2), 10)
 
-# Helper function to draw a dot
 def drawDot(screen, center, alpha):
     inner_radius = 5  # Radius for the inner circle
     outer_radius = 10  # Radius for the outer circle
     inner_color = (20, 20, 20, alpha)
     outer_color = (60, 60, 60, alpha)
 
-    # Draw the outer circle
     p.draw.circle(screen, outer_color, center, outer_radius)
-
-    # Draw the inner circle
     p.draw.circle(screen, inner_color, center, inner_radius)
 
-'''
-Draw the pieces
-'''
 def drawPieces(screen, board):
     for r in range(DIMENTIONS):
         for c in range(DIMENTIONS):
@@ -231,9 +191,6 @@ def drawPieces(screen, board):
             if piece != "--":
                 screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
-'''
-Draw move log
-'''
 def drawMoveLog(screen, gs, font):
     moveLogRect= p.Rect(BOARD_WIDTH, 0, MOVE_LOG_PANEL_WIDTH, MOVE_LOG_PANEL_HEIGHT)
     p.draw.rect(screen, p.Color(50, 50, 50), moveLogRect)
